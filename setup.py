4f0aa2bfcdb995f2403128b75c1fff230e42afb4
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2023-04-12 14:49:31 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="openapify",
-    version="0.3.1",
+    version="0.3.2",
     description=(
         "Framework agnostic OpenAPI Specification generation for code lovers"
     ),
